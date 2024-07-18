f6fe591a523ebe09f06d31099a005576644aa236
Alina-Pishak <alinapishak@icloud.com>
2024-07-18 16:04:46 +0300

    remove everything

--- src/components/ContactFields/ContactFields.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ContactFields = () => {
-  return <div>ContactFields</div>;
-};
-
-export default ContactFields;
--- src/components/ContactOperations/ContactOperations.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ContactOperations = () => {
-  return <div>ContactOperations</div>;
-};
-
-export default ContactOperations;
